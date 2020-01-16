068c40d23b229ebc9823de50f411e34001caf91b
Bogdan <bogdan@medve-dev.net>
2020-01-16 06:13:36 +0300

    UserInterfaceStyle changed to SystemAppearance.
    UserInterfaceStyle is deprecated use SystemAppearance instead.
    https://docs.nativescript.org/api-reference/modules/_ui_enums_.userinterfacestyle
    https://docs.nativescript.org/api-reference/modules/_ui_enums_.systemappearance

--- src/dark-mode.ios.ts
+++ src/dark-mode.ios.ts
@@ -1,6 +1,5 @@
 import * as app from "tns-core-modules/application";
 import { device } from "tns-core-modules/platform";
-import { topmost } from "tns-core-modules/ui/frame";
 import { _addOnDarkModeChangedListener, _setDarkModeStyleSheet, emitIfModeChangedEvent } from "./dark-mode.common";
 
 (() => {
@@ -22,6 +21,6 @@ export function isDarkModeSupported(): boolean {
 }
 
 export function isDarkModeEnabled(): boolean {
-  return topmost().viewController.traitCollection.userInterfaceStyle === 2;
+  return app.systemAppearance() === 'dark';
 }
 
